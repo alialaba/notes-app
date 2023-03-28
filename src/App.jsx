9c0a79df1d2cd66d57e257e0f4b7e84d50eb5cbf
alialaba <alialaba079@gmail.com>
2023-03-28 10:32:41 -0400

    added starter files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+
+export default function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
