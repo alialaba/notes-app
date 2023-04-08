13aa6d2cce1a96c5879d5a37461b20e17d57e2d1
alialaba <alialaba079@gmail.com>
2023-04-07 23:35:12 -0400

    ft: implement create note and note ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,88 @@
 import { useState } from 'react'
+import Editor from "./components/Editor"
+import Sidebar from './components/Sidebar'
+import Split from 'react-split';
+import {nanoid} from "nanoid"
+//react mde  style
+import "react-mde/lib/styles/css/react-mde-all.css";
 
 
 export default function App() {
-  const [count, setCount] = useState(0)
+  const [notes, setNotes] = useState([]);
+  // check if notes[0] exists Before getting notes[0].id
+  const [curNoteId, setCurNoteId] = useState((notes[0]  && notes[0].id) || "");
+
+  /*
+  task 1: display sidebar and editor when their is a note .
+  task 1b: display editor only if user is on the currentNoteId && 
+  task 2: As a user, I want to be abble to create new note (this can be achieve using  id: nanoid(),
+            body: "# Type your markdown note's title here")
+  task 3 : As a user i want to be to update my note (tips using paramter of text and value of body: text);
+  task 4: display current note on the editor (findcurrentnote) || display first note 
+  
+  */
+
+//function that update note 
+const updateNote =(text)=>{
+  setNotes(oldNotes => oldNotes.map(oldNote =>(
+
+    //if oldnote id equals to currNoteId
+    oldNote.id === curNoteId ? {...oldNote, body: text} : oldNote
+  )))
+}
+
+  // Create a new note function
+
+  const createNewNote = ()=>{
+    const newNote ={
+      id: nanoid(),
+      body:"# Type your markdown note title here"
+    }
+    setNotes(prevNotes => [newNote , ...prevNotes]);
+    setCurNoteId(newNote.id)
+  }
+
+  function findCurrentNote (){
+    return notes.find(note=>{
+      return note.id === curNoteId }) || notes[0] ;
+  }
+ 
 
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <main >
+
+      {notes.length > 0 ?
+
+<Split
+sizes={[25, 75]}
+direction="horizontal" 
+className="split">
+
+<Sidebar 
+newNote={createNewNote} 
+currentNote={findCurrentNote()} 
+setCurNoteId={setCurNoteId} 
+notes={notes} />
+
+{curNoteId &&  notes.length > 0 && 
+  <Editor 
+  currentNote={findCurrentNote()} 
+  updateNote={updateNote} />
+}
+</Split>
+:
+      
+<div className="no-notes">
+<h1>You have no notes</h1>
+<button className="first-note" onClick={createNewNote}>
+    Create one now
+</button>
+</div>
+      
+      }
+      
+     
+    </main>
   )
 }
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,24 @@
-export default function(){
+export default function({newNote, notes, currentNote, setCurNoteId}){
+
+    const noteElements = notes.map((note,index)=>(
+        <div key={note.id}>
+           <div className={`title ${note.id === currentNote.id ? "selected-note" : ""}`}
+
+            onClick={()=> setCurNoteId(note.id)}>
+           <h4 className="text-snippet">note {index + 1} </h4>
+           </div>
+        </div>
+    ))
+
+    
     return(
-        <div></div>
+       
+        <aside className="sidebar pane">
+            <div className="sidebar__header">
+                <h3 className="sidebar__title">Notes</h3>
+                <button className="sidebar__new-note" onClick={newNote}>+</button>
+            </div>
+            {noteElements}
+        </aside>
     );
 }
