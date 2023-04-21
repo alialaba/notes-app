cfcc58fac80fbfe681d2a579a065ee11c327feb8
alialaba <alialaba079@gmail.com>
2023-04-20 23:59:53 -0400

    ft implement show more notes and style custom scrollbar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,29 +1,46 @@
+import { useState } from "react";
 import img from "../assets/icons8-note.svg"
+import { useMemo } from "react";
+
 export default function({newNote, notes, currentNote, setCurNoteId, deleteNote}){
+    const [numberOfItemShown, setNumberOfItemShown] = useState(7);
 
-    const noteElements = notes.map((note,index)=>(
-        <div key={note.id}>
-           <div className={`title ${note.id === currentNote.id ? "selected-note" : ""}`}
+const showMore=()=>{
+    if(numberOfItemShown <= notes.length){
+        setNumberOfItemShown( numberOfItemShown + 5)
+    }else{
+        setNumberOfItemShown(notes.length)
+    }
+}
 
-            onClick={()=> setCurNoteId(note.id)}>
-               <img src={img}/>
-           <h4 className="text-snippet">{note.body.split("\n")[0]} </h4>
-           
-           <button  className="delete-btn" onClick={(event) => deleteNote(event, note.id)} >
-                    <i className="gg-trash trash-icon"></i>
-                </button>
-           </div>
-        </div>
-    ))
+
+    const noteElements = useMemo(()=>(
+
+        notes.slice(0, numberOfItemShown).map((note,index)=>(
+            <div key={note.id}>
+               <div className={`title ${note.id === currentNote.id ? "selected-note" : ""}`}
+    
+                onClick={()=> setCurNoteId(note.id)}>
+                   <img src={img}/>
+               <h4 className="text-snippet">{note.body.split("\n")[0]} </h4>
+               
+               <button  className="delete-btn" onClick={(event) => deleteNote(event, note.id)} >
+                        <i className="gg-trash trash-icon"></i>
+                    </button>
+               </div>
+            </div>
+        ))
+
+    )) 
 
     return(
        
         <aside className="sidebar pane">
             <div className="sidebar__header">
-                {/* <h3 className="sidebar__title">Notes</h3> */}
                 <button className="sidebar__new-note" onClick={newNote}>+ New Note</button>
             </div>
-            {noteElements}
+            {noteElements.length ? noteElements : "loading..."}
+           <button className="show-more" onClick={showMore}>show more</button> 
            
         </aside>
     );
